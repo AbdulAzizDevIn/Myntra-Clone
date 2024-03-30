d7d38e2cb607d36c91c83cb503110a6be8a7d4bb
Abdul Aziz <azizskwwe@gmail.com>
2024-03-30 20:24:47 +0530

    connect to backend

---
+++ Backend/Routes/DisplayData.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.post("/bannerData",(req,res) => {
+    try{
+        res.send([global.banners])
+    }
+    catch(err){
+        console.error(err.message);
+        res.send("Server Error")
+    }
+})
+router.post("/productsData",(req, res) => {
+    try{
+        res.send([global.products]);
+    }
+    catch(err){
+        console.error(err.message);
+        res.send("Server Error")
+    }
+})
+
+module.exports = router;

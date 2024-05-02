a6ee94b795e724a03da11e28893deb6048612aed
Abdul Aziz <95928540+Aziz7477@users.noreply.github.com>
2024-05-02 23:05:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
   
 const fetchProductData =()=>{
   fetch("https://myntra-clone-api.vercel.app/api/productsData",{
-    method:"POST",
+    method:"GET",
     headers:{
       "Content-Type": "application/json"
     }
